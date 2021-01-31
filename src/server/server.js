20b2db5ed1cd24f85d4a4f5bdad573b78c2e4fff
Promise Fru <promisefru1@gmail.com>
2021-01-31 23:33:44 +0100

    fixed client on disconnect event

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,10 @@ const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     console.log('client connected');
-});
 
-io.on('disconnect', (socket) => {
-    console.log('client disconnected');
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
 });
 
 server.listen(3000, console.log(`waiting for clients ...`));
